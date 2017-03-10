21aa511e659ad261144b6a6b6a62f6f60bfa7dcf
OrigenesZhang <zhangbin199807@gmail.com>
2017-03-10 14:01:47 +0800

    Update UVa674.cpp

--- UVa674.cpp
+++ UVa674.cpp
@@ -3,7 +3,7 @@ using namespace std;
 const int maxn=7489;
 int coins[5]={1, 5, 10, 25, 50}, table[5][7500], num;
 int main(){
-    for(int i=0; i<=maxn; i++) table[0][i] = 1;
+    for(int i=0; i<=maxn; i++) table[0][i]=1;
     for(int i=0; i<5; i++)
         table[1][i]=table[2][i]=table[3][i]=table[4][i]=1;
     for(int i=1; i<5; i++)
