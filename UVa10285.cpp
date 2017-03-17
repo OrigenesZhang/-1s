897e03e882f5a9a7ccb4fe38f59e1461a711c7ac
OrigenesZhang <zhangbin199807@gmail.com>
2017-03-17 18:40:47 +0800

    Update UVa10285.cpp

--- UVa10285.cpp
+++ UVa10285.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 string s;
 int N, R, C, dp[105][105], table[105][105], mmax;
-int bfs(int x, int y){
+int dfs(int x, int y){
     if(x<1||y<1||x>R||y>C) return -100;//写搜索的时候要注意小错误，不要想当然地作方表处理
     if(dp[x][y]!=-1) return dp[x][y];
     for(int i=-1; i<=1; i++){
         for(int j=-1; j<=1; j++){
             if(i==j||i*j==-1) continue;
             if(table[x+i][y+j]>=table[x][y]) continue;
-            dp[x][y]=max(bfs(x+i, y+j)+1, dp[x][y]);
+            dp[x][y]=max(dfs(x+i, y+j)+1, dp[x][y]);
         }
     }
     if(dp[x][y]==-1) dp[x][y]=1;
@@ -28,7 +28,7 @@ int main(){
         mmax=-1;
         for(int i=1; i<=R; i++)
             for(int j=1; j<=C; j++)
-                if(dp[i][j]==-1) bfs(i, j);
+                if(dp[i][j]==-1) dfs(i, j);
         cout<<s<<": "<<mmax<<endl;
     }
     return 0;
