fe88d8ac84d6e5f04b08757aaff4bfba309df5c5
OrigenesZhang <zhangbin199807@gmail.com>
2017-08-19 12:37:55 +0800

    Create 08WF_B.py

---
+++ 08WF_B.py
@@ -0,0 +1,26 @@
+case=0
+while True:
+    case+=1
+    s=input()
+    if '.' in s:
+        break
+    p, d=s.split('/')
+    d=int(d)
+    p=p.replace('^','**')
+    ch=''
+    for i in range(0, len(p)-1):
+        if p[i].isdigit() and p[i+1].isalpha():
+            ch=ch+p[i]+'*'
+        else:
+            ch=ch+p[i]
+    ch=ch+')'
+    f=True
+    for i in range(1, 102):
+        n=i
+        exec('ans='+ch)
+        if ans%d:
+            f=False
+            print('Case %d: Not always an integer' % case)
+            break
+    if f:
+        print('Case %d: Always an integer' % case)
