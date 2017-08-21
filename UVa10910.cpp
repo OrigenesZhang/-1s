bd10b88093f8e5018237b411e6b1de1f6f0a0e24
OrigenesZhang <zhangbin199807@gmail.com>
2017-08-21 12:54:09 +0800

    Create UVa10910.cpp

---
+++ UVa10910.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int k, n, t, p;
+long long combinaton(long long n, long long r){
+    r=min(r, n-r);
+    long long c=1;
+    for(long long i=1; i<=r; i++)
+        c=c*(n-i+1)/i;
+    return c;
+}
+int main(){
+    cin>>k;
+    while(k--){
+        cin>>n>>t>>p;
+        int r=t-n*p;
+        cout<<combinaton(n+r-1, n-1);
+    }
+    return 0;
+}
