eb772a3a50e239835ea8ef381e8c3a92fce9b4ae
OrigenesZhang <22521689+OrigenesZhang@users.noreply.github.com>
2018-05-13 23:42:32 +0800

    Create CF101623B.cpp

---
+++ CF101623B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORD(i,a,b) for (int i = (a); i >= (b); i--)
+#define REP(i,a) FOR(i,0,(int)(a)-1)
+#define reset(a,b) memset(a,b,sizeof(a))
+#define all(x) x.begin(), x.end()
+#define uni(x) x.erase(unique(all(x)), x.end());
+#define BUG(x) cout << #x << " = " << x << endl
+#define BUGP(x) cout << #x << " = " << x._1 << ", " << x._2 << endl
+#define PR(x,a,b) {cout << #x << " = "; FOR (_,a,b) cout << x[_] << ' '; cout << endl;}
+#define CON(x) {cout << #x << " = "; for(auto _i:x) cout << _i << ' '; cout << endl;}
+#define mod 1000000007
+#define pi acos(-1)
+#define eps 1e-7
+#define pb push_back
+#define sqr(x) (x) * (x)
+#define _1 first
+#define _2 second
+
+int n;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin >> n;
+  cout << max(1, n - 2);
+}
