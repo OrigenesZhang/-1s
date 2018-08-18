b3cf133d7794d3885829a0fe639858b2323b4e2c
OrigenesZhang <22521689+OrigenesZhang@users.noreply.github.com>
2018-08-18 15:00:03 +0800

    找规律

---
+++ Maths/GameTheory/UVa847.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+#define FOR(i, a, b) for (int (i) = (a); (i) <= (b); (i)++)
+#define ROF(i, a, b) for (int (i) = (a); (i) >= (b); (i)--)
+#define REP(i, n) FOR(i, 0, (n)-1)
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end());
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define pb push_back
+#define mp make_pair
+#define _1 first
+#define _2 second
+
+int main() {
+    double n;
+    while (scanf("%lf", &n) == 1) {
+        while (n > 18) n /= 18;
+        puts(n <= 9 ? "Stan wins." : "Ollie wins.");
+    }
+    return 0;
+}
